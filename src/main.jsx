bddb75b12db2865f05df8b7f8d524cae0da26917
newal77 <waltanewal@gmail.com>
2024-12-09 09:55:01 -0700

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { DataProvider } from "./components/DataProvider/DataProvider.jsx";
-import { initialState, reducer } from "./Utility/reducer.jsx";
+import { initialState, reducer } from "./utility/reducer.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
